35f92c1abd6eec9405a2a1daac514934cbc8a038
JanuszMarek <janusz.marek92@gmail.com>
2019-02-15 14:25:40 +0100

    Add CartSummaryViewComponent

---
+++ SportStoreCore/Component/CartSummaryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStoreCore.Models;
+
+namespace SportsStoreCore.Component
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+
+        public CartSummaryViewComponent(Cart cartservice)
+        {
+            cart = cartservice;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
