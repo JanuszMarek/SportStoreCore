23987d9b754c299a1260e6496f5583086766dff6
JanuszMarek <janusz.marek92@gmail.com>
2019-02-21 08:18:46 +0100

    Starting Creating CV form

---
+++ Lessons/Areas/CV/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lessons.Areas.CV.Models;
+using Lessons.Areas.CV.Models.ViewModels;
+
+namespace Lessons.Areas.CV.Controllers
+{
+    [Area("CV")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewModel viewModel = new ViewModel();
+            viewModel.PersonalInfo.FirstName = "Janusz";
+            viewModel.PersonalInfo.LastName = "Marek";
+            viewModel.PersonalInfo.Birthdate = DateTime.Parse("09-05-1992");
+            viewModel.PersonalInfo.Email = "janusz.marek92@gmail.com";
+            viewModel.PersonalInfo.GitHub = "https://github.com/JanuszMarek";
+            viewModel.PersonalInfo.LinkedIn = "https://www.linkedin.com/in/janusz-marek/";
+            viewModel.PersonalInfo.StreetNo = "ul. Odrowążów 93a/5";
+            viewModel.PersonalInfo.ZipCode = "44-103 Gliwice";
+
+            viewModel.Categories.Add(new Category
+            {
+                Name = "Wykształcenie",
+                CategoryItems = new List<CategoryItem>()
+                {
+                    new CategoryItem
+                    {
+                        Title = "Politechnika Śląska w Gliwicach",
+                        SubTitle = "Wydz. Automatyki Informatyki i Elektroniki",
+                        Description = "Automatyka i Robotyka Magister (Mgr)",
+                        PhotoUrl = "",
+                        Years = "2016 – 2017"
+                    },
+                    new CategoryItem
+                    {
+                        Title = "Politechnika Śląska w Gliwicach",
+                        SubTitle = "Wydz. Automatyki Informatyki i Elektroniki",
+                        Description = "Automatyka i Robotyka Inżynier (Inż.)",
+                        PhotoUrl = "",
+                        Years = "2012 – 2016"
+                    },
+                }
+            });
+
+
+
+            return View();
+        }
+    }
+}
---
+++ Lessons/Areas/CV/Models/ViewModels/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lessons.Areas.CV.Models.ViewModels
+{
+    public class ViewModel
+    {
+        public PersonalInfo PersonalInfo { get; set; }
+        public List<Category> Categories { get; set; } = new List<Category>();
+        public string[] Skills { get; set; }
+        public string[] Languages { get; set; }
+    }
+}
